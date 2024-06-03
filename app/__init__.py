d00613a53cd728e93f41c260753f17b0271255e0
Alexey Chernous <139914095+HGRaicer@users.noreply.github.com>
2024-06-03 15:32:25 +0300

    bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -7,32 +7,13 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-
-app.config["SECRET_KEY"] = (
-    os.environ.get("SECRET_KEY") or "you-will-never-guess"
-)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://postgres:1234@localhost:5432/Web_pizza_DB"
-)
-
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy()
-db.init_app(app)
-migrate = Migrate(app, db)
-
-login = LoginManager(app)
-login.login_view = "login"
-
-
-app = Flask(__name__)
 # Устанавливаем секретный ключ для приложения, используя переменную окружения или значение по умолчанию
 app.config["SECRET_KEY"] = (
     os.environ.get("SECRET_KEY") or "you-will-never-guess"
 )
 # Устанавливаем URI для подключения к PostgreSQL
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") or (
-    "postgresql://postgres:1234@localhost:5432/Web_pizza_DB"
+    "postgresql://postgres:1234@localhost:5432/web_pizza_db"
 )
 
 # Отключаем отслеживание изменений SQLAlchemy, что улучшает производительность
